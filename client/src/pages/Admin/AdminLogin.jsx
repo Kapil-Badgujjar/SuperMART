de911c58321d69c2901d2158dc7976b8b7d71aca
Kapil-Badgujjar <k.b.cool.boy99@gmail.com>
2023-10-25 10:42:31 +0530

    Bug fixes

--- client/src/pages/Admin/AdminLogin.jsx
+++ client/src/pages/Admin/AdminLogin.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react'
 import Button from '../../components/Button/Button'
 import styles from './AdminLogin.module.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { login, logout } from '../../features/admin/adminSlice'
+import { useDispatch, useSelector } from 'react-redux'
 export default function AdminLogin() {
+  const navigate = useNavigate();
+  const dispath = useDispatch();
   const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMessage] = useState(undefined);
   async function getAdmin(event) {
     event.preventDefault()
     try{
       const response = await axios.post(import.meta.env.VITE_SERVER_ADDRESS+'/admin/login', { password: password}, { withCredentials: true});
+      dispath(login());
+      if(response.status === 200){
+        
+        navigate('/admin/dashboard');
+      }
       console.log(response);
     } catch (error){
-      console.log(error.message);
+      setErrorMessage(error.message);
+      setTimeout(()=> {setErrorMessage(undefined);},3000);
     }
   }
   return (
     <div className={styles.admin_login}>
         <form className={styles.login_form}>
-            <h2>Admin</h2>
-            <p className={styles.error}>*Wrong id/password</p>
+            <h2>Admin Login</h2>
+            <p className={styles.error}>{errorMsg && '*Wrong id/password'}</p>
             <input className={styles.input_box} vlaue={password} type='password' placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} />
             <Button value="Login" action={getAdmin} />
             <hr />
