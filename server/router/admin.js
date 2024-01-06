813cc41f4509423c69d4d393f49eff3970d4fb58
Kapil Badgujjar <kapil.badgujjar@jethitech.com>
2024-01-06 10:51:42 +0530

    minor updates

--- server/router/admin.js
+++ server/router/admin.js
@@ -1,23 +1,24 @@
+import {} from 'dotenv/config'
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid';
+import jwt from 'jsonwebtoken';
 const router = express.Router()
 
 router.route('/login').post((req, res) => {
-    console.log(req.body);
-    res.status(200).send({ message: "Success!" });
+    const admin = {
+        name: 'Kapil Badgujjar',
+        id: 10001
+    }
+    if(req.body.password === process.env.ADMIN_PASSWORD){
+        const token = jwt.sign(admin, process.env.ADMIN_SECRET_KEY, { expiresIn: '30m'});
+        res.status(200).send({ message: "Success!", token: token });
+        return;
+    }
+    res.status(400).send({ message: "Session Expired!" });
 });
 
 router.route('/get-seller').get((req, res) => {
-    res.status(200).send([
-        {
-            "name": "Kapil",
-            "id": "02038",
-            "items": "44",
-            "registrationDate": "20-12-2023",
-            "isVerified": "true",
-            "isActive": "true",
-            "isBlocked": "false"
-        }
-    ])
+
 });
 
 export default router;
